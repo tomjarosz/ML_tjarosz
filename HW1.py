851b7c4048c1109ff23b286b391a34cc4fb2cf06
Tom Jarosz <tjarosz@uchicago.edu>
2016-04-01 16:55:00 -0500

    hw1

--- HW1.py
+++
@@ -1,8 +0,0 @@
-### First Assignment
-import pandas as pd
-import numpy as np
-
-data = pd.read_csv('mock_student_data.csv')
-students = pd.DataFrame(data, columns = ['ID','First_name','Last_name','State','Gender','Age','GPA','Days_missed','Graduated'])
-
-print(students['Gender'].describe())
