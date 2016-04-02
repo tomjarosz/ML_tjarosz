36351f0e118baf038d0b163ad1b14e76647b2f39
Tom Jarosz <tjarosz@uchicago.edu>
2016-04-02 17:56:29 -0500

    HW1 draft

--- HW1.py
+++ HW1.py
@@ -18,24 +18,53 @@ df = pd.DataFrame(data, )
 # print("Missing Data")
 # print((len(df.index)) - df.count())
 
-def genderize(df):
+# def genderize(df):
 
-    no_gender = df[df['Gender'].isnull()]['First_name']
-    for name in no_gender:
-        gender = gender_api(name)
+#     no_gender = df[df['Gender'].isnull()]['First_name']
+#     for name in no_gender:
+#         gender = gender_api(name)
 
-        df.loc[df['Gender'].isnull(),'Gender'] = gender
+#         df.loc[df['Gender'].isnull(),'Gender'] = gender
 
-    df.to_csv('genders_filled_in')    
+#     return df
+#     df.to_csv('genders_filled_in.csv')    
         
 
-def gender_api(first_name):
+# def gender_api(first_name):
 
-    webservice_url = "https://api.genderize.io/?name=" + first_name
-    gender_data = json.loads(urllib.request.urlopen(webservice_url).read().decode("utf8"))
-    return gender_data["gender"]
+#     webservice_url = "https://api.genderize.io/?name=" + first_name
+#     gender_data = json.loads(urllib.request.urlopen(webservice_url).read().decode("utf8"))
+#     return gender_data["gender"]
 
 
-print(genderize(df))    
+# # print(genderize(df))    
+
+# def missing_values_means (df):
+#     age_mean = round((df['Age'].mean()), 1)
+#     GPA_mean = round((df['GPA'].mean()), 1)
+#     days_missed_mean = round((df['Days_missed'].mean()), 1)
+
+#     df['Age'].fillna(age_mean, inplace = True)
+#     df['GPA'].fillna(GPA_mean, inplace = True)
+#     df['Days_missed'].fillna(days_missed_mean, inplace = True)
+
+#     df.to_csv('missing_values_means.csv')    
+
+def missing_values_cond_means (df):
+
+    graduated = df[df['Graduated'] == 'Yes'][['Age', 'GPA', 'Days_missed']]
+    age_mean = round(graduated['Age'].mean(), 1)
+    GPA_mean = round(graduated['GPA'].mean(), 1)
+    days_missed_mean = round(graduated['Days_missed'].mean(), 1)
+
+    df['Age'].fillna(age_mean, inplace = True)
+    df['GPA'].fillna(GPA_mean, inplace = True)
+    df['Days_missed'].fillna(days_missed_mean, inplace = True)
+
+    df.to_csv('missing_values_cond_means.csv')  
+
+missing_values_cond_means(df)
+
+
 
 
