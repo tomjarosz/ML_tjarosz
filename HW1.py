9132f723728315e1f2116dbf60fe2893e35e4e1a
Tom Jarosz <tjarosz@uchicago.edu>
2016-04-04 00:15:10 -0500

    HW1 12:45

--- HW1.py
+++ HW1.py
@@ -40,57 +40,55 @@ def summary_stats(df):
     stats = pd.DataFrame(data, rows)
     stats.to_csv('descriptive_statistics.csv')   
         
-# def gender_api(first_name):
+def gender_api(first_name):
 
-#     webservice_url = "https://api.genderize.io/?name=" + first_name
-#     gender_data = json.loads(urllib.request.urlopen(webservice_url).read().decode("utf8"))
-#     return gender_data["gender"]
+    webservice_url = "https://api.genderize.io/?name=" + first_name
+    gender_data = json.loads(urllib.request.urlopen(webservice_url).read().decode("utf8"))
+    return gender_data["gender"]
 
-# def genderize(df):
+def genderize(df):
 
-#     no_gender = df[df['Gender'].isnull()]['First_name']
-#     for name in no_gender:
-#         gender = gender_api(name)
-#         df.loc[(df['Gender'].isnull()) & (df['First_name'] == name),'Gender'] = gender
+    no_gender = df[df['Gender'].isnull()]['First_name']
+    for name in no_gender:
+        gender = gender_api(name)
+        df.loc[(df['Gender'].isnull()) & (df['First_name'] == name),'Gender'] = gender
 
-#     df.to_csv('genders_filled_in.csv')
+    df.to_csv('genders_filled_in.csv')
 
-# def missing_values_means (df):
-#     age_mean = round((df['Age'].mean()), 1)
-#     GPA_mean = round((df['GPA'].mean()), 1)
-#     days_missed_mean = round((df['Days_missed'].mean()), 1)
+def missing_values_means (df):
+    age_mean = round((df['Age'].mean()), 1)
+    GPA_mean = round((df['GPA'].mean()), 1)
+    days_missed_mean = round((df['Days_missed'].mean()), 1)
 
-#     df['Age'].fillna(age_mean, inplace = True)
-#     df['GPA'].fillna(GPA_mean, inplace = True)
-#     df['Days_missed'].fillna(days_missed_mean, inplace = True)
+    df['Age'].fillna(age_mean, inplace = True)
+    df['GPA'].fillna(GPA_mean, inplace = True)
+    df['Days_missed'].fillna(days_missed_mean, inplace = True)
 
-#     df.to_csv('missing_values_means.csv')
+    df.to_csv('missing_values_means.csv')
 
-# def missing_values_cond_means (df):
+def missing_values_cond_means (df):
 
-#     graduated = df[df['Graduated'] == 'Yes'][['Age', 'GPA', 'Days_missed']]
-#     age_grad_mean = round(graduated['Age'].mean(), 1)
-#     GPA_grad_mean = round(graduated['GPA'].mean(), 1)
-#     days_missed_grad_mean = round(graduated['Days_missed'].mean(), 1)
+    graduated = df[df['Graduated'] == 'Yes'][['Age', 'GPA', 'Days_missed']]
+    age_grad_mean = round(graduated['Age'].mean(), 1)
+    GPA_grad_mean = round(graduated['GPA'].mean(), 1)
+    days_missed_grad_mean = round(graduated['Days_missed'].mean(), 1)
 
-#     df.loc[(df['Graduated'] == 'Yes') & (df['Age'].isnull()),'Age'] = age_grad_mean
-#     df.loc[(df['Graduated'] == 'Yes') & (df['GPA'].isnull()),'GPA'] = GPA_grad_mean
-#     df.loc[(df['Graduated'] == 'Yes') & (df['Days_missed'].isnull()),'Days_missed'] = days_missed_grad_mean
+    df.loc[(df['Graduated'] == 'Yes') & (df['Age'].isnull()),'Age'] = age_grad_mean
+    df.loc[(df['Graduated'] == 'Yes') & (df['GPA'].isnull()),'GPA'] = GPA_grad_mean
+    df.loc[(df['Graduated'] == 'Yes') & (df['Days_missed'].isnull()),'Days_missed'] = days_missed_grad_mean
 
-#     no_graduated = df[df['Graduated'] == 'No'][['Age', 'GPA', 'Days_missed']]
-#     age_nograd_mean = round(no_graduated['Age'].mean(), 1)
-#     GPA_nograd_mean = round(no_graduated['GPA'].mean(), 1)
-#     days_missed_nograd_mean = round(no_graduated['Days_missed'].mean(), 1)
+    no_graduated = df[df['Graduated'] == 'No'][['Age', 'GPA', 'Days_missed']]
+    age_nograd_mean = round(no_graduated['Age'].mean(), 1)
+    GPA_nograd_mean = round(no_graduated['GPA'].mean(), 1)
+    days_missed_nograd_mean = round(no_graduated['Days_missed'].mean(), 1)
 
-#     df.loc[(df['Graduated'] == 'No') & (df['Age'].isnull()),'Age'] = age_nograd_mean
-#     df.loc[(df['Graduated'] == 'No') & (df['GPA'].isnull()),'GPA'] = GPA_nograd_mean
-#     df.loc[(df['Graduated'] == 'No') & (df['Days_missed'].isnull()),'Days_missed'] = days_missed_nograd_mean
+    df.loc[(df['Graduated'] == 'No') & (df['Age'].isnull()),'Age'] = age_nograd_mean
+    df.loc[(df['Graduated'] == 'No') & (df['GPA'].isnull()),'GPA'] = GPA_nograd_mean
+    df.loc[(df['Graduated'] == 'No') & (df['Days_missed'].isnull()),'Days_missed'] = days_missed_nograd_mean
     
-#     df.to_csv('missing_values_cond_means.csv')
+    df.to_csv('missing_values_cond_means.csv')
 
-# def drop_missing_data(df):
+def drop_missing_data(df):
     
-#     df = df[(df['Age'].notnull()) & (df['GPA'].notnull()) & (df['Days_missed'].notnull())]
-#     df.to_csv('values_with_complete_data.csv')
-
-# drop_missing_data(df)    
+    df = df[(df['Age'].notnull()) & (df['GPA'].notnull()) & (df['Days_missed'].notnull())]
+    df.to_csv('values_with_complete_data.csv')
