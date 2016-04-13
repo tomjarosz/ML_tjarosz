b5f612db6923a267cd5c23ef8733df5dd197f517
Tom Jarosz <tjarosz@uchicago.edu>
2016-04-12 23:17:03 -0500

    PA2 Completed

--- PA2/models.py
+++ PA2/models.py
@@ -9,6 +9,13 @@ from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 
 def prep_model_data(df_train, dep_vars, indep_vars):
+    '''
+    Prepare data for evaluating various models, using the 
+    training dataset, list of indepedent variables, and 
+    dependent variables.
+
+    Utilizes train_test_split function for parse data
+    '''
     y_train = df_train[dep_vars]
     y_train = np.ravel(y_train)
     x_train = df_train[indep_vars]
@@ -17,6 +24,10 @@ def prep_model_data(df_train, dep_vars, indep_vars):
     return(x_train, x_validate, y_train, y_validate)
 
 def logit_model(x_train, x_validate, y_train, y_validate):
+    '''
+    Fits and predicts a model for the data using
+    the parsed data from the train_test_split function
+    '''
     #could pass in a list of models and loop through them to fit and predict probs
     logit_model = LogisticRegression()
     logit_model.fit(x_train, y_train)
@@ -24,11 +35,19 @@ def logit_model(x_train, x_validate, y_train, y_validate):
     return pred_probs, logit_model  
 
 def evaluate(y_validate, pred_probs):
+    '''
+    Evaluates the fit of the model on the validation data 
+    using an accuracy metric.
+    '''
+
     accuracy = metrics.accuracy_score(y_validate, pred_probs)
     return accuracy
 
 def score_data(logit_model, df_test, dep_vars, indep_vars):
-    
+    '''
+    Utilizes the best model to predict outcomes in the 
+    testing dataset
+    '''
     x_test = df_test[indep_vars]
     y_test = logit_model.predict_proba(x_test)
 
